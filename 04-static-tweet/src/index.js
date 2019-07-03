31ece107688712c9dcdd4889731cb59892650ed0
Dave Ceddia <dceddia@gmail.com>
2019-07-02 20:46:44 -0400

    renamed NameWithHandle -> Author

--- 04-static-tweet/src/index.js
+++ 04-static-tweet/src/index.js
@@ -5,15 +5,16 @@ import './index.css';
 function Tweet() {
   return (
     <div className="tweet">
-      <Avatar/>
+      <Avatar />
       <div className="content">
-        <NameWithHandle/><Time/>
-        <Message/>
+        <Author />
+        <Time />
+        <Message />
         <div className="buttons">
-          <ReplyButton/>
-          <RetweetButton/>
-          <LikeButton/>
-          <MoreOptionsButton/>
+          <ReplyButton />
+          <RetweetButton />
+          <LikeButton />
+          <MoreOptionsButton />
         </div>
       </div>
     </div>
@@ -21,16 +22,12 @@ function Tweet() {
 }
 
 function Message() {
-  return (
-    <div className="message">
-      This is less than 280 characters.
-    </div>
-  );
+  return <div className="message">This is less than 280 characters.</div>;
 }
 
-function NameWithHandle() {
+function Author() {
   return (
-    <span className="name-with-handle">
+    <span className="author">
       <span className="name">Your Name</span>
       <span className="handle">@yourhandle</span>
     </span>
@@ -42,29 +39,21 @@ function Avatar() {
     <img
       src="https://www.gravatar.com/avatar/nothing"
       className="avatar"
-      alt="avatar" />
+      alt="avatar"
+    />
   );
 }
 
-const Time = () => (
-  <span className="time">3h ago</span>
-);
+const Time = () => <span className="time">3h ago</span>;
 
-const ReplyButton = () => (
-  <i className="fa fa-reply reply-button"/>
-);
+const ReplyButton = () => <i className="fa fa-reply reply-button" />;
 
-const RetweetButton = () => (
-  <i className="fa fa-retweet retweet-button"/>
-);
+const RetweetButton = () => <i className="fa fa-retweet retweet-button" />;
 
-const LikeButton = () => (
-  <i className="fa fa-heart like-button"/>
-);
+const LikeButton = () => <i className="fa fa-heart like-button" />;
 
 const MoreOptionsButton = () => (
-  <i className="fa fa-ellipsis-h more-options-button"/>
+  <i className="fa fa-ellipsis-h more-options-button" />
 );
 
-ReactDOM.render(<Tweet/>,
-  document.querySelector('#root'));
+ReactDOM.render(<Tweet />, document.querySelector('#root'));
