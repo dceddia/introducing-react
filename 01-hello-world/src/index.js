08c3ce34f020d4455e25d9aa081a56ae4a54c500
Dave Ceddia <dceddia@gmail.com>
2019-07-09 14:48:31 -0400

    Updated Hello World 'after' to match the lesson

--- 01-hello-world/src/index.js
+++ 01-hello-world/src/index.js
@@ -5,11 +5,20 @@ function HelloWorld() {
   const name = 'Dave';
 
   return (
-    <div>
-      <h1>This is a Hello</h1>
-      hello <strong>{name}</strong>
-    </div>
+    <>
+      <div>
+        <h1>This is a {name} Hello</h1>
+        hello
+        <strong>
+          {name + '!'} {1 + 2}
+        </strong>
+      </div>
+      <span>another element</span>
+    </>
   );
 }
 
-ReactDOM.render(<HelloWorld />, document.querySelector('#root'));
+ReactDOM.render(
+  <HelloWorld />,
+  document.querySelector('#root')
+);
