9e4289bf793033da46b2889ca4997953a36a3270
Dave Ceddia <dceddia@gmail.com>
2019-07-09 19:25:45 -0400

    Update 02-after to match the lesson

--- 02-createElement/src/index.js
+++ 02-createElement/src/index.js
@@ -1,19 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function Custom() {
+  return <span>custom</span>;
+}
+
 function HelloWorld() {
   const name = 'Dave';
 
   return React.createElement(
     'div',
-    {},
-    React.createElement('h1', {}, 'This is a Hello'),
+    null,
+    React.createElement('h1', null, 'This is a Hello'),
     'hello ',
-    React.createElement('strong', {}, name)
+    React.createElement('strong', null, name),
+    React.createElement(Custom)
+  );
+  return (
+    <div>
+      <h1>This is a Hello</h1>
+      hello <strong>{name}</strong>
+      <Custom />
+    </div>
   );
 }
 
 ReactDOM.render(
-  React.createElement(HelloWorld),
+  <HelloWorld />,
   document.querySelector('#root')
 );
